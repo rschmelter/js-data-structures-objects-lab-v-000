02a1803008cf05edcdac205395036f6183aa0bfb
Ryan Schmelter <rschmelter33@gmail.com>
2018-08-06 23:32:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,12 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
   return Object.assign(driver, {[key]: value});
+}
 
+function deleteFromDriverByKey(driver, key) {
+  const newDriver = Object.assign({}, driver)
+
+  delete newDriver[key]
+
+  
 }
